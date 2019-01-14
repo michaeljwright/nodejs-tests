0831145dd31195c405733232fb04f605fedfbaa3
Mike <mike.filmworks@gmail.com>
2019-01-14 16:38:10 +0000

    Updated for dynamic routes & Advanced models
    section: 9

--- models/product.js
+++ models/product.js
@@ -2,16 +2,19 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
-const getProductsFromFile = callback => {
+const getProductsFromFile = cb => {
 
     fs.readFile(p, (err, fileContent) => {
 
         if(err) {
-            return callback([]);
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
         }
-        callback(JSON.parse(fileContent));
 
     });
 
@@ -19,8 +22,8 @@ const getProductsFromFile = callback => {
 
 module.exports = class Product {
 
-    constructor(title, imageUrl, description, price) {
-
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -32,10 +35,37 @@ module.exports = class Product {
 
         getProductsFromFile(products => {
 
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-                if(err) {
-                    console.log(err);
+            if(this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            }
+
+        });
+
+    }
+
+    static deleteById(id) {
+
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                if(!err) {
+                    Cart.deleteProduct(id, product.price);
                 }
             });
 
@@ -43,9 +73,20 @@ module.exports = class Product {
 
     }
 
-    static fetchAll(callback) {
+    static fetchAll(cb) {
 
-        getProductsFromFile(callback);
+        getProductsFromFile(cb);
+
+    }
+
+    static findById(id, cb) {
+
+        getProductsFromFile(products => {
+
+            const product = products.find(p => p.id === id);
+            cb(product);
+
+        });
 
     }
 
--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,11 @@ router.get('/add-product', adminController.getAddProduct);
 
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 router.get('/products', adminController.getProducts);
 
